f941b4389da9d14e71264b741cd3cf879dfbd43b
JaeMinSSong <ssong_1015@naver.com>
2024-09-07 07:15:59 +0900

    테스트 코드 연습

--- src/main/java/org/example/grade/ui/ConsoleOutputUI.java
+++
@@ -1,9 +0,0 @@
-package org.example.grade.ui;
-
-import org.example.grade.domain.GradeResult;
-
-public class ConsoleOutputUI {
-    public static void printGrade(GradeResult gradeResult) {
-        System.out.println("이번 학기 학점: " + gradeResult.getAverageGrade() + " (총 " + gradeResult.getTotalCompletedCredit() + "학점)");
-    }
-}
--- src/main/java/org/example/hr/domain/EmployRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.hr.domain;
-
-public interface EmployRepository {
-    void createEmployee(Employee employee);
-
-    Employee findByNo(String no);
-}
