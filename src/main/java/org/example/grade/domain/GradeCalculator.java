a987c2b276349cfd9863521137207a023e0a9121
JaeMinSSong <ssong_1015@naver.com>
2024-09-07 22:25:19 +0900

    일급 컬렉션 개념(Ex.학점 계산기)

--- src/main/java/org/example/grade/domain/GradeCalculator.java
+++ src/main/java/org/example/grade/domain/GradeCalculator.java
@@ -16,5 +16,6 @@ public class GradeCalculator {
         double averageGrade = totalMultipliedCreditAndCourseGrade / totalCompletedCredit;
 
         return new GradeResult(averageGrade, totalCompletedCredit);
+
     }
 }
